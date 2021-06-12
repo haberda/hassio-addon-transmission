801c7729613354e15d9ac535424e9d515bb1bece
haberda <25726974+haberda@users.noreply.github.com>
2021-06-12 13:32:54 -0700

    Update up-transmission.sh

--- rootfs/etc/openvpn/up-transmission.sh
+++ rootfs/etc/openvpn/up-transmission.sh
@@ -8,8 +8,6 @@ CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"0.0.0.0\"")
 
 echo "${CONFIG}" > /data/transmission/settings.json
 
-exec /usr/bin/transmission-daemon --foreground --config-dir /data/transmission
-
 export localNet="192.168.0.0/16"
 
 eval $(/sbin/ip route list match 0.0.0.0 | awk '{if($5!="tun0"){print "GW="$3"\nINT="$5; exit}}')
@@ -19,3 +17,5 @@ echo "adding route to local network ${localNet} via ${GW} dev ${INT}"
 /sbin/ip route del "$localNet"
 
 /sbin/ip route add "${localNet}" via "${GW}" dev "${INT}"
+
+exec /usr/bin/transmission-daemon --foreground --config-dir /data/transmission
